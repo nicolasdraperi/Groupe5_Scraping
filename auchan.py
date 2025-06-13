2af32ab9f7c58db2e0837d00cd87bf53f9776449
Nicolas Draperi <nicolas.draperi2@gmail.com>
2025-06-13 12:49:48 +0200

    Recupération du code barre et du prix de l'objet

--- auchan.py
+++ auchan.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-
 url = "https://www.auchan.fr/magasins/drive/auchan-drive-la-trinite-nice-cote-d-azur/s-869"
 driver = webdriver.Chrome()
 driver.get(url)
@@ -29,11 +28,20 @@ try:
     time.sleep(0.5)
     search_input.clear()
     time.sleep(0.5)
-    search_input.send_keys("3124480191182")
+    search_input.send_keys("3248830002284")
     search_input.send_keys(Keys.ENTER)
     print("recherche lancée pour item")
 except:
     print("barre de recherche non trouvée.")
+
+try:
+    price_element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "div.product-price.bolder.text-dark-color"))
+    )
+    price = price_element.text.strip()
+    print(f"prix détecté : {price}")
+except:
+    print("prix non trouvé.")
 
 input("appuie sur Entrée pour fermer...")
 driver.quit()
--- scraping.py
+++ scraping.py
@@ -5,30 +5,43 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-driver = webdriver.Chrome()
-driver.get("https://world.openfoodfacts.org/")
+def get_barcode(nom_produit):
+    driver = webdriver.Chrome()
+    driver.get("https://world.openfoodfacts.org/")
 
-search_input = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.NAME, "search_terms"))
-)
+    search_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "search_terms"))
+    )
 
-search_input.click()
-time.sleep(0.5)
-search_input.clear()
-time.sleep(0.5)
-search_input.send_keys("oasis")
-time.sleep(0.5)
-search_input.send_keys(Keys.ENTER)
-time.sleep(2.5)
-product_list = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID, "products_match_all"))
-)
+    search_input.click()
+    time.sleep(0.5)
+    search_input.clear()
+    time.sleep(0.5)
+    search_input.send_keys(nom_produit)
+    time.sleep(0.5)
+    search_input.send_keys(Keys.ENTER)
 
-premier_produit = product_list.find_element(By.TAG_NAME, "li")
-time.sleep(1.2)
-premier_produit.click()
+    time.sleep(2.5)
+    product_list = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "products_match_all"))
+    )
 
-print("produit cliqué avec succès.")
+    premier_produit = product_list.find_element(By.TAG_NAME, "li")
+    time.sleep(1.2)
+    premier_produit.click()
+    print("Produit cliqué")
 
-input("appuie sur Entrée pour fermer...")
-driver.quit()
+    try:
+        barcode_elem = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "barcode"))
+        )
+        barcode = barcode_elem.text.strip()
+        print(f"code-barres récupéré : {barcode}")
+    except:
+        print("probleme de recuperation le code-barres.")
+        barcode = None
+
+    driver.quit()
+    return barcode
+
+get_barcode("carrote")
